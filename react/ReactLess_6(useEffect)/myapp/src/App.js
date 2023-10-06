5eaaaa4a1a986e77804fbe798c6d290efd9f847b
Ilya Kotovskiy <ilundrik026@gmail.com>
2023-10-06 14:24:33 +0300

    small marks

---
+++ react/ReactLess_6(useEffect)/myapp/src/App.js
@@ -0,0 +1,15 @@
+import Example from "./components/Example";
+// useEffect - это функция, которая следит за каждым этапом жизненного цикла компонента и 
+// выполняет побочный код
+// пример смотреть в компоненте Example
+
+
+function App() {
+  return (
+    <div>
+      <Example/>
+    </div>
+  );
+}
+
+export default App;
